555143c6736f0a7b6569a8a7f71a6a8bc37c505f
shirasu-info <heart_of_thoma@yahoo.co.jp>
2026-01-10 00:00:56 +0900

    change template

--- js/common.js
+++ js/common.js
@@ -1,38 +1,56 @@
 // スマホ用メニュー　クラス追加
 const ham = document.querySelector("#js-hamburger");
 const nav = document.querySelector("#js-globalnav");
+const Main = document.querySelector("#js-main");
 
 ham.addEventListener("click", function () {
-  ham.classList.toggle("_active");
-  nav.classList.toggle("_active");
+ ham.classList.toggle("_active");
+ nav.classList.toggle("_active");
+ Main.classList.toggle("_darker");
 });
 
+// 子メニュー表示
+const parentMenu = document.querySelectorAll("._has-child > a");
+for (let i = 0; i < parentMenu.length; i++) {
+ parentMenu[i].addEventListener("click", function(e){
+  e.preventDefault();
+  this.nextElementSibling.classList.toggle("active");
+ })
+}
 
- // COMIC用メニュー　クラス追加
- const comicbtn = document.querySelector("#js-comicnav-btn");
- const comicnav = document.querySelector("#js-comicnav");
- 
- comicbtn?.addEventListener("click", function () {
-   comicbtn.classList.toggle("_active");
-   comicnav.classList.toggle("_active");
+// ページUP
+const PageUpBtn = document.getElementById('js-pageup');
+
+window.addEventListener("scroll", function () {
+ const scroll = window.pageYOffset;
+ if (scroll > 700) {
+  PageUpBtn.style.opacity = 1;
+ } else PageUpBtn.style.opacity = 0;
+});
+
+PageUpBtn?.addEventListener('click', () => {
+ window.scrollTo({
+  top: 0,
+  behavior: 'smooth'
  });
- 
+});
 
- // スムーススクロール
- const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
- for (let i = 0; i < smoothScrollTrigger.length; i++){
-   smoothScrollTrigger[i].addEventListener('click', (e) => {
-     e.preventDefault();
-     let href = smoothScrollTrigger[i].getAttribute('href');
-      let targetElement = document.getElementById(href.replace('#', ''));
-     const rect = targetElement?.getBoundingClientRect().top;
-     const offset = window.pageYOffset;
-     const gap = 0;
-     const target = rect + offset - gap;
-     window.scrollTo({
-       top: target,
-       behavior: 'smooth',
-     });
-   });
- }
+// キャラクター紹介タブ
+// ボタン
+const nameList = document.querySelectorAll(".charanamelist__item");
+// コンテンツ
+const charaContent = document.querySelectorAll(".charalist__item");
 
+document.addEventListener("DOMContentLoaded", function () {
+  for (let i = 0; i < nameList.length; i++) {
+    nameList[i].addEventListener("click", charaSwitch);
+  }
+  function charaSwitch() {
+    document.querySelectorAll(".active")[0]?.classList.remove("active");
+    this?.classList.add("active");
+    document.querySelectorAll(".show")[0].classList.remove("show");
+    const aryList = Array.prototype.slice.call(nameList);
+    const index = aryList.indexOf(this);
+    charaContent[index].classList.add("show");
+  }
+});
